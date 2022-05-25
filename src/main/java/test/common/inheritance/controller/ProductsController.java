d548619567f04da654b11961e73a80dbb0c782e9
AIELLO Lorenzo <lorenzo.aiello@soprasteria.com>
2022-05-25 17:32:15 +0200

    remove warning on imports

--- src/main/java/test/common/inheritance/controller/ProductsController.java
+++ src/main/java/test/common/inheritance/controller/ProductsController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import test.common.inheritance.entity.sons.Book;
